a3632c0c5daba62bd795481ee755106ec5540c0f
System Administrator <kevinqi34>
2016-04-29 20:38:40 -0400

    update

--- Template/FAdBlockLeft.php
+++ Template/FAdBlockLeft.php
@@ -3,8 +3,7 @@
 echo <<<HEREDOC
 
 <div id="adblockleft">
-<script type="text/javascript">document.write("<scr" + "ipt type='text/javascript' src='http://www.epicgameads.com/ads/popjs.php?pchsh=35b1c4e6cb75cc5915dd605d6a503a51&cb=" + new Date().getTime() + "'></scri" + "pt>");</script>
-
+<script type="text/javascript" src="//go.oclasrv.com/apu.php?zoneid=631659"></script>
 
 </div>
 
