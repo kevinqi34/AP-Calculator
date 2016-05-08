8fe85fc09984167d2fe36c00711cdc1603d7f200
System Administrator <kevinqi34>
2016-05-08 18:19:52 -0400

    update

--- Template/FAdBlockTop.php
+++ Template/FAdBlockTop.php
@@ -12,7 +12,6 @@ var ad_idzone = "2023586",
 <script type="text/javascript" src="https://ads.exoclick.com/ads.js"></script>
 <noscript><a href="http://main.exoclick.com/img-click.php?idzone=2023586" target="_blank"><img src="https://syndication.exoclick.com/ads-iframe-display.php?idzone=2023586&output=img&type=728x90" width="728" height="90"></a></noscript>
 
-
 </div>
 
 HEREDOC;
