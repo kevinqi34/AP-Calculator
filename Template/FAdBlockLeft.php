637fbd1b28267a6c672b2c7e052013965186948b
System Administrator <kevinqi34>
2016-05-08 18:53:57 -0400

    update

--- Template/FAdBlockLeft.php
+++ Template/FAdBlockLeft.php
@@ -4,6 +4,8 @@ echo <<<HEREDOC
 
 <div id="adblockleft">
 
+<iframe style="width:120px;height:240px;margin-left: calc(50% - 60px);" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=spere-10-20&marketplace=amazon&region=US&placement=1438005008&asins=1438005008&linkId=6DL5QPCRNHUPRM5C&show_border=true&link_opens_in_new_window=true">
+</iframe>
 
 </div>
 
