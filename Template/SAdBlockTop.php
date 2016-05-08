a1e7f0d18cb20066e0c7c80f9073395a0b4e6567
System Administrator <kevinqi34>
2016-05-08 18:58:54 -0400

    update

--- Template/SAdBlockTop.php
+++ Template/SAdBlockTop.php
@@ -3,13 +3,6 @@
 echo <<<HEREDOC
 
 <div id="adblocktop">
-<script type="text/javascript">
-var ad_idzone = "2023586",
-	 ad_width = "728",
-	 ad_height = "90";
-</script>
-<script type="text/javascript" src="https://ads.exoclick.com/ads.js"></script>
-<noscript><a href="http://main.exoclick.com/img-click.php?idzone=2023586" target="_blank"><img src="https://syndication.exoclick.com/ads-iframe-display.php?idzone=2023586&output=img&type=728x90" width="728" height="90"></a></noscript>
 
 
 <!-- Mobicow.com Ad Code Start : Do Not Modify -->
