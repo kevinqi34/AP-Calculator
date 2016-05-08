63fc999a565ffb564946b3921abc44df1ba7a493
System Administrator <kevinqi34>
2016-05-08 18:32:42 -0400

    update

--- Template/FAdBlockTop.php
+++ Template/FAdBlockTop.php
@@ -12,6 +12,20 @@ var ad_idzone = "2023586",
 <script type="text/javascript" src="https://ads.exoclick.com/ads.js"></script>
 <noscript><a href="http://main.exoclick.com/img-click.php?idzone=2023586" target="_blank"><img src="https://syndication.exoclick.com/ads-iframe-display.php?idzone=2023586&output=img&type=728x90" width="728" height="90"></a></noscript>
 
+
+<!-- Mobicow.com Ad Code Start : Do Not Modify -->
+<script>
+    var mc_s1 = document.createElement("script");
+    var _mcpv = _mcpv || {};
+    _mcpv.triggers = ["exit","move","link"];
+    mc_s1.async = true;
+    mc_s1.src = "http://cdn.mobicow.com/deliver/p/12349/9989/1/over/900";
+    var MCs_1 = document.getElementsByTagName("script")[0];
+    MCs_1.parentNode.insertBefore(mc_s1, MCs_1);
+</script>
+<!-- Mobicow.com Ad Code End : Do Not Modify -->
+
+
 </div>
 
 HEREDOC;
