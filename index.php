5a8ddbf23f033b22223f1a73098ab415d37517de
System Administrator <kevinqi34>
2016-05-09 17:27:37 -0400

    update

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $menu->pushtoArray("AP.jpg|GPA.jpg|IB.jpg|test.png|sats.png", "urlList");
 
 $title = 'Score Calculators and Tools';
 $keywords = 'Advanced Placement Calculator, IB diploma Calculator, SAT/ACT Calculator, SAT subject tests Calculator';
+
 $affiliate = '<iframe style="width:120px;height:240px;margin-left:calc(50% - 60px);margin-top:150px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=spere-10-20&marketplace=amazon&region=US&placement=1438002696&asins=1438002696&linkId=V5P3P2GZMQGGMFAZ&show_border=true&link_opens_in_new_window=true"></iframe>';
 
 
