3cbb294f69ebeda4aaf58520351e9c5e99a7d0b6
System Administrator <kevinqi34>
2016-05-09 10:50:00 -0400

    update

--- Template/SAdBlockLeft.php
+++ Template/SAdBlockLeft.php
@@ -8,16 +8,11 @@ echo <<<HEREDOC
   var screenWidth = window.screen.width;
 
 if (screenWidth > 1350) {
-  $('#adblockleft').html('
-  <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=spere-10-20&marketplace=amazon&region=US&placement=1438005008&asins=1438005008&linkId=6DL5QPCRNHUPRM5C&show_border=true&link_opens_in_new_window=true">
-  </iframe>');
+  $('#adblockleft').html('<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=spere-10-20&marketplace=amazon&region=US&placement=1438005008&asins=1438005008&linkId=6DL5QPCRNHUPRM5C&show_border=true&link_opens_in_new_window=true"></iframe>');
 
 
 }else {
-  $('#adblockleft').html('
-  <a href="http://www.tkqlhce.com/click-8066581-10941828-1391449830000" target="_top">
-  <img src="http://www.awltovhc.com/image-8066581-10941828-1391449830000" width="300" height="250" alt="" border="0"/></a>
-  ');
+  $('#adblockleft').html('<a href="http://www.tkqlhce.com/click-8066581-10941828-1391449830000" target="_top"><img src="http://www.awltovhc.com/image-8066581-10941828-1391449830000" width="300" height="250" alt="" border="0"/></a>');
 }
 
 </script>
