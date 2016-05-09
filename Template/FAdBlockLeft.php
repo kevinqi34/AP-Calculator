46e48a6d3ac6e42f257aa5b276a7d582820d892d
System Administrator <kevinqi34>
2016-05-09 10:51:13 -0400

    update

--- Template/FAdBlockLeft.php
+++ Template/FAdBlockLeft.php
@@ -6,6 +6,7 @@ echo <<<HEREDOC
 
 <script>
   var screenWidth = window.screen.width;
+  alert(screenWidth);
 
 if (screenWidth > 1350) {
   $('#adblockleft').html('<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=spere-10-20&marketplace=amazon&region=US&placement=1438005008&asins=1438005008&linkId=6DL5QPCRNHUPRM5C&show_border=true&link_opens_in_new_window=true"></iframe>');
