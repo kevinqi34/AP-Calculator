e7c75069aad913645280923feb7c78d3c3ddd455
System Administrator <kevinqi34>
2016-05-11 15:14:44 -0400

    update

--- Template/FAdBlockLeft.php
+++ Template/FAdBlockLeft.php
@@ -11,7 +11,7 @@ $(window).on("load resize",function(e){
 
 
 if (screenWidth > 1350) {
-$('#adblockleft').html('\<script type="text/javascript" src="//ap.lijit.com/www/delivery/fpi.js?z=354987&u=kevinqi34&width=160&height=600"></script>');
+$('#adblockleft').html('<script type="text/javascript" src="//ap.lijit.com/www/delivery/fpi.js?z=354987&u=kevinqi34&width=160&height=600"></script>');
 
   }else {
   $("#adblockleft").html('<a href="http://www.tkqlhce.com/click-8066581-10941828-1391449830000" target="_top"><img src="http://www.awltovhc.com/image-8066581-10941828-1391449830000" width="300" height="250" alt="" border="0"/></a>');
